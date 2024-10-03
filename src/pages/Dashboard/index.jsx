115c3737821d6851407a0bf2e537c85c79a06380
ValdecirArruda <valdecirrogerio@hotmail.com>
2024-10-02 23:00:04 -0300

    commit1

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -2,14 +2,40 @@ import { useState } from "react";
 
 import { FaSearch } from "react-icons/fa";
 
+import { getUserDetails } from "../../api/github";
+
 import { Container, Content } from './styles'
 
 export const Dashboard = () => {
-  const [par1, setPar1] = useState(0)
+  const[username, setUserName] = useState('')
+  const [loading, setLoading] = useState(false)
+  
+  function handleSetUser(event) {
+    setUserName(Event.target.value)
+  }
+
+  async function handleGetDetaisl() {
+    try {
+      setLoading(true)
+      const result = await getUserDetails(username)
+
+      console.log(result)
+
+    } catch (error) {
+      console.error(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+  
   return (
     <Container>
       <Content>
-        <input type="text" />
+        <label htmlFor="nickname">
+          Digite o usúario GIT
+          <input id="nickname" type="text" placeholder="Digite aqui seu usúario"/>
+        </label>
+       
 
         <button>Buscar <FaSearch /></button>
       </Content>
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,11 +1,62 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  height: 100vh;  
+  display: flex;  
+  flex-direction: column;
+  align-items: center;
 `
 
 export const Content = styled.div`
-  display: flex;  
-  justify-content: center;
-  align-items: center;
+width:40%;
+margin-top:64px;
+
+display:flex;
+justify-content:center;
+align-items:center;
+
+label {
+  display:flex;
+  flex-direction:column;
+  font-size:10px;
+  width:100%;
+
+  input {
+    margin-top:4px;
+    padding:8px;
+    border:1px solid ${({theme}) => theme.primary};
+    background:${({theme}) => theme.primary_light};
+    border-radius:4px;
+
+    &::placeholder {
+      color: ${({theme}) => theme.gray};
+    }
+  }
+}
+
+button {
+  width:100%;
+  height:32px;
+  margin-top:16px;
+  background: ${({theme}) =>theme.primary};
+  border:0;
+  border-radius:4px;
+  font-weight:500;
+
+  display:flex;
+  justify-content:center;
+  align-items:center;
+
+  margin-left:8px;
+
+  svg {
+    margin-left:8px;
+    background:transparent;
+  }
+  
+  transition: all 0.3s;
+  &:hover {
+    transform: scale(1.03);
+  }
+}
+
 `
